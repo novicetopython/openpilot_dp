ed06798875b6408c8ffa21425e9684a8341ceaaa
novicetopython <35001563+novicetopython@users.noreply.github.com>
2025-12-06 15:47:06 +0000

    Update board.h

--- panda/board/stm32h7/board.h
+++ panda/board/stm32h7/board.h
@@ -22,12 +22,14 @@ void detect_board_type(void) {
   const uint8_t id1 = detect_with_pull(GPIOF, 7, PULL_UP) |
                      (detect_with_pull(GPIOF, 8, PULL_UP) << 1U) |
                      (detect_with_pull(GPIOF, 9, PULL_UP) << 2U) |
-                     (detect_with_pull(GPIOF, 10, PULL_UP) << 3U);
+                     (detect_with_pull(GPIOF, 10, PULL_UP) << 3U) |
+                     (GPIOB, 15, PULL_UP << 4U);
 
   const uint8_t id2 = detect_with_pull(GPIOD, 4, PULL_UP) |
                      (detect_with_pull(GPIOD, 5, PULL_UP) << 1U) |
                      (detect_with_pull(GPIOD, 6, PULL_UP) << 2U) |
-                     (detect_with_pull(GPIOD, 7, PULL_UP) << 3U);
+                     (detect_with_pull(GPIOD, 7, PULL_UP) << 3U) |
+                     (GPIOB, 15, PULL_UP << 4U);
 
   if (id2 == 3U) {
     hw_type = HW_TYPE_CUATRO;
@@ -42,6 +44,9 @@ void detect_board_type(void) {
   } else if (id1 == 2U) {
     hw_type = HW_TYPE_TRES;
     current_board = &board_tres;
+  } else if (id1 == 4U) {
+    hw_type = HW_TYPE_DOS;
+    current_board = &board_dos;
   } else {
     hw_type = HW_TYPE_UNKNOWN;
     print("Hardware type is UNKNOWN!\n");
